2d4314671a3a8207a894589210c92bc9a3ee3d29
Riya Dutta <riya2001dutta@gmail.com>
2023-07-29 16:23:04 +0530

    little upgrade

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+dotenv.config({path:'./config.env'});
+
+const homeStartingContent = "Hello there, and welcome to my digital realm of coding wonders! I'm Riya, and I'm thrilled to have you here. If you share a fascination for the art of coding and the limitless possibilities it brings, then you're in the right place. This blog is all about unraveling the magic of programming, one line at a time. Step into the enchanting world of coding where ideas become reality, and imagination knows no bounds.";
+const aboutContent = "Hey there! I'm Riya Dutta, a passionate coder and technology enthusiast. I have always been captivated by the way software and technology can shape our world and make it a better place. This blog is my little corner of the internet where I get to share my thoughts, experiences, and insights about the fascinating world of coding. I believe that coding is not just a skill but a creative art that empowers us to bring our ideas to life. Through this platform, I aim to inspire others, especially beginners, to take their first steps into the coding world and discover the wonders it offers.";
+const contactContent = "Thank you for visiting, Coding Chronicles! I'm thrilled that you're interested in reaching out to me. Whether you have a question, feedback, collaboration proposal, or just want to say hello, I'd love to hear from you. Connecting with like-minded individuals is one of the things that make the coding community so special, so don't hesitate to get in touch!";
 
 const app = express();
 
@@ -15,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://riyadutta:blogpass@blog.fuxa8wq.mongodb.net/blogDB", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
@@ -79,10 +82,8 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+const PORT = process.env.PORT;
 
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
